1ce0085d17a5d6a56af6cec637c1efb155c52be0
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-03-13 01:14:44 +0000

    Updates

---
+++ precache-manifest.e15dcbfe8c908bd4d7379a7a615432f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Random-Quote-Machine-React-freeCodeCamp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2424d440e26840413629",
+    "url": "/Random-Quote-Machine-React-freeCodeCamp/static/js/runtime~main.32978d18.js"
+  },
+  {
+    "revision": "8acda07aec012e1da7b1",
+    "url": "/Random-Quote-Machine-React-freeCodeCamp/static/js/main.0af2c23b.chunk.js"
+  },
+  {
+    "revision": "18e659cad06ecd3435b8",
+    "url": "/Random-Quote-Machine-React-freeCodeCamp/static/js/2.a00790a1.chunk.js"
+  },
+  {
+    "revision": "8acda07aec012e1da7b1",
+    "url": "/Random-Quote-Machine-React-freeCodeCamp/static/css/main.c1d5d64d.chunk.css"
+  },
+  {
+    "revision": "18e659cad06ecd3435b8",
+    "url": "/Random-Quote-Machine-React-freeCodeCamp/static/css/2.182e0588.chunk.css"
+  },
+  {
+    "revision": "bd296dd10d82fec5e087192c3ca646c2",
+    "url": "/Random-Quote-Machine-React-freeCodeCamp/index.html"
+  }
+];
